b4486e9c54db0d4d78ef71a40ab5f48ef8d18f69
rallmac2 <root@tobijahdevice>
2024-05-30 23:44:22 +0100

    square

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -5,6 +5,7 @@
 ###
 
 result = []
+
 ARGV.each do |arg|
     # skip if not integer
     next if arg !~ /^-?[0-9]+$/
@@ -12,24 +13,9 @@ ARGV.each do |arg|
     # convert to integer
     i_arg = arg.to_i
     result << i_arg
-
-    # convert to character
-    char_arg = arg[0]
-
-    # insert result at the right position
-    is_inserted = false
-    i = 0
-    l = result.size
-    while is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
-        end
-    end
-    result << i_arg if !is_inserted
+  
 end
+    
+result.sort!
 
-puts result
+puts result.join(' ')
