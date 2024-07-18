5d9770930cfe8b59b2822f4b992069a13e5ed432
rallmac2 <root@tobijahdevice>
2024-07-18 12:59:25 +0100

    task 0 redo

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -4,18 +4,18 @@ Web server
 """
 from api.v1.views import app_views
 from urllib.parse import quote as url_quote
-from flask import Flask, jsonify, make_response
+from flask import Flask
+import sys
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-
-@app.errorhandler(404)
-def not_found(error):
-    """ json 404 page """
-    return make_response(jsonify({"error": "Not found"}), 404)
-
+def run():
+    host = '0.0.0.0'
+    port = 5000
+    print(f" * Running on http://{host}:{port}/ (Press CTRL+C to quit)", file=sys.stderr)
+    app.run(host=host, port=port, threaded=True)
 
 if __name__ == "__main__":
     # python -m api.v1.app 
-    app.run(host="0.0.0.0", port=5000)
+    run()
